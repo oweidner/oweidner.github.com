f0bd467e74df00204004d905c495e2c303d1a80b
Ole Weidner <oweidner@cct.lsu.edu>
2010-02-10 09:21:38 -0600

    added missing header

---
+++ header.js
@@ -0,0 +1,15 @@
+document.write(" \
+\
+  <div id=\"header\" class=\"basic\">\
+    <div class=\"site\">\
+      <!--<div class=\"logo\">\
+        <a href=\"/\"><img src=\"/images/modules/header/logov3.png\" alt=\"git-hub\" /></a>\
+      </div>-->\
+\
+      <div class=\"actions\">\
+        <a href=\"/\">Home</a> <a href=\"/publications\">Publications</a> <a href=\"/projects\">Projects</a>\
+      </div>\
+    </div>\
+  </div>\
+\
+");
