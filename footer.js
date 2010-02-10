c224d28284b4f487d4bf5c2cc99077aaf9addc20
Ole Weidner <oweidner@Teahupoo.local>
2010-02-10 07:55:15 -0600

    added footer

---
+++ footer.js
@@ -0,0 +1,25 @@
+document.write(" \
+<br><br>\
+    <div id=\"footer\">\
+      <div class=\"site\">\
+\
+        <div class=\"info\">\
+          <div class=\"links\">\
+            <a href=\"http://cct.lsu.edu\">CCT</a> |\
+            <a href=\"contact.html\">Contact</a> \
+          </div>\
+          <div class=\"company\">\
+            &copy; 2010 Ole Weidner. All rights reserved \
+          </div>\
+        </div>\
+        <div class=\"sponsor\">\
+          <div>\
+            \"Put something smart here.\"\
+          </div>\
+          <!--<a href=\"http://www.rackspace.com\">\
+            <img src=\"http://github.com/images/modules/footer/rackspace_logo.png\" alt=\"Dedicated Server\" />-->\
+          </a>\
+        </div>\
+      </div>\
+    </div>\
+");
